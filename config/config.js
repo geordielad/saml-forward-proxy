d2656e3ce875b41a2fbde97fe6141354457f443d
geordielad <rcottiss@cottiss.com>
2017-06-19 15:00:13 -0400

    Clean up config.js

--- config/config.js
+++ config/config.js
@@ -14,8 +14,7 @@ module.exports = {
         entryPoint: process.env.SAML_ENTRY_POINT || 'https://dev-221604.oktapreview.com/app/tableausoftwaredev221604_tableau_1/exk61mg6poMGQ8Fnk0h7/sso/saml', 
 	      authnRequestBinding: process.env.SAML_AUTHN_REQUEST_BINDING || 'HTTP-Redirect', // Change to HTTP-POST if required
         issuer: 'https://saml_sp_entityid', // DYNAMIC FROM ORININAL REQUEST - See routes.js
-        //privateCert: process.env.SAML_PRIVATE_CERT || fs.readFileSync('./tableau_ami_sp.key', 'utf-8'),
-        //skipRequestCompression: true, // Optional depending on IdP
+       //skipRequestCompression: true, // Optional depending on IdP
         //acceptedClockSkewMs: -1, // Optional depending on IdP
         //disableRequestedAuthnContext: true, //Optional depending on IdP
         //privateCert: process.env.SAML_PRIVATE_CERT || fs.readFileSync('./tableau_ami_sp.key', 'utf-8'), //Uncomment if Request Signing is required.
