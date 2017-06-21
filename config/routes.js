dadeffe4c3fbffd6f4cc039dbf622bfb62a978b4
geordielad <rcottiss@cottiss.com>
2017-06-21 10:08:02 -0400

    Cleaned up Routes

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,6 @@
 module.exports = function (app, config, passport, util, samlEncodeDecode, xml2js, xpath) {
 
+/*
   app.get('/', function (req, res) {
     if (req.isAuthenticated()) {
       res.render('home',
@@ -13,28 +14,7 @@ module.exports = function (app, config, passport, util, samlEncodeDecode, xml2js
         });
     }
   });
-
-  app.get('/login', function(req, res, next) {
-    passport.authenticate(config.passport.strategy,
-      {
-        successRedirect: '/',
-        failureRedirect: '/login'
-        //additionalParams: {'RelayState': req.query.RelayState}
-      })(req,res,next);
-  });
-
-  app.post(config.passport.saml.path,
-    passport.authenticate(config.passport.strategy,
-      {
-        failureRedirect: '/',
-        failureFlash: true
-      }),
-    function (req, res) {
-      var proxyHost = 'https://ttproxy.tableau.rocks';
-      //res.redirect('/');
-      res.redirect(proxyHost+'/login-to-tableau-server/'+req.user.email+'?redirect=&site=');
-    }
-  );
+*/
 
   app.post('/saml_proxy', function(req, res, next) {
 
@@ -70,27 +50,6 @@ module.exports = function (app, config, passport, util, samlEncodeDecode, xml2js
         callbackUrl: AssertionConsumerServiceURL,
         issuer: issuer
       })(req, res, next); // <- just remember to add these
-  });
-
-  app.get('/signup', function (req, res) {
-    res.render('signup');
-  });
-
-  app.get('/profile', function (req, res) {
-    if (req.isAuthenticated()) {
-      res.render('profile',
-        {
-          user: req.user
-        });
-    } else {
-      res.redirect('/login');
-    }
-  });
-
-  app.get('/logout', function (req, res) {
-    req.logout();
-    // TODO: invalidate session on IP
-    res.redirect('/');
   });
 
 };
